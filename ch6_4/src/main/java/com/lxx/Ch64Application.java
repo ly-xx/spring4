218cdfb4189682df1d63072661888162b794eb23
廖晓霞 <liaoxx@daqsoft.com.cn>
2019-06-04 17:12:28 +0800

    profile

---
+++ ch6_4/src/main/java/com/lxx/Ch64Application.java
@@ -0,0 +1,13 @@
+package com.lxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch64Application.class, args);
+	}
+
+}
